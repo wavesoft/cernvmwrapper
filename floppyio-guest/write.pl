dcbd72f84f2c8758eb1e64b583120407f6f232b4
wavesoft <johnys2@gmail.com>
2012-01-06 16:45:31 +0200

    Added license on guest scripts

--- floppyio-guest/write.pl
+++ floppyio-guest/write.pl
@@ -31,6 +31,23 @@
 #
 # Created on November 24, 2011, 12:30 PM
 # Author: Ioannis Charalampidis <ioannis.charalampidis AT cern DOT ch>
+# License: GNU/LGPL Version 3
+#
+# This file is part Floppy I/O, a Virtual Machine - Hypervisor intercommunication system.
+# Copyright (C) 2011 Ioannis Charalampidis 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #######################################################################
 
